37e43090b05d980b163b4aa3c573ba088c2cec14
Joshua Boyd <boyd.jc.github@gmail.com>
2022-06-15 22:01:07 -0400

    users can successfully sign in with their email and password

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -309,4 +309,41 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+
+  # Turbo doesn't work with devise by default. 
+  # Keep tabs on https://github.com/heartcombo/devise/issues/5446 for a possible fix
+  # Fix from https://gorails.com/episodes/devise-hotwire-turbo
+  class TurboFailureApp < Devise::FailureApp
+    def respond
+      if request_format == :turbo_stream
+        redirect
+      else
+        super
+      end
+    end
+
+    def skip_format?
+      %w(html turbo_stream */*).include? request_format.to_s
+    end
+  end
+
+
+  # ...
+  Devise.setup do |config|
+    # ...
+  
+    # ==> Controller configuration
+    # Configure the parent class to the devise controllers.
+    config.parent_controller = 'DeviseTurboController'
+  
+    # ...
+
+    # ==> Warden configuration
+    # ...
+    config.warden do |manager|
+      manager.failure_app = TurboFailureApp
+    #   manager.intercept_401 = false
+    #   manager.default_strategies(scope: :user).unshift :some_external_strategy
+    end
+  end
 end
